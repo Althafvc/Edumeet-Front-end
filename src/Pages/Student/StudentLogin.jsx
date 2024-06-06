62f557d8fc2f66044d954bf9651ed8bba07e1e53
Althaf vc <althafvellanchola46@gmail.com>
2024-06-06 19:11:27 +0530

    some changes and redirection added in student login

--- src/Pages/Student/StudentLogin.jsx
+++ src/Pages/Student/StudentLogin.jsx
@@ -5,11 +5,16 @@ import Image from '../../assets/images/Studentlogin.png';
 import DefaultButton from '../../Components/Student/DefaultButton';
 import axiosInstance from '../../Instance/Axiosinstance';
 import BasicAlerts from '../../Components/BasicAlerts';
+import { useNavigate } from 'react-router-dom';
 
+// Main functional component for student login
 const StudentLogin = () => {
+  
+  const Navigate = useNavigate()
+  // State to handle error visibility, type, and message
+  const [error, setError] = useState({ visible: false, type: '', msg: '' })
 
-  const [error, setError] = useState({ visibl: false, type: '', msg: '' })
-
+  // Validation schema using Yup for form validation
   const validationSchema = Yup.object({
     email: Yup.string()
       .required('Email is required')
@@ -25,12 +30,20 @@ const StudentLogin = () => {
       )
   });
 
+  if(error) {setTimeout(() => {
+    setError('')
+  }, 1000);}
+
   return (
     <div className="content w-screen h-screen flex justify-center items-center bg-white">
       <div className="form-container px-2 md:px-0 rounded-2xl md:w-2/4 md:h-[480px] flex flex-col md:flex-row md:shadow-customShadow overflow-auto md:overflow-hidden h-auto">
+        
+        {/* Left side with welcome image */}
         <div className="md:py-16 welcome-area md:w-[50%] h-full flex justify-center items-center flex-col gap-5 md:gap-16 overflow-hidden">
           <img src={Image} alt="image not found" className="md:w-full md:h-full w-auto h-auto" />
         </div>
+        
+        {/* Right side with login form */}
         <div className="form-area bg-[#ffffff] md:w-[50%] md:h-full md:flex md:justify-center md:flex-col md:gap-10 md:items-center md:py-10 flex flex-col justify-center w-full gap-10 py-8">
           <div className="heading-area flex justify-center">
             <h2 className="text-[#4b4343] font-bold md:text-2xl text-xl text-center">Login to your account</h2>
@@ -43,18 +56,15 @@ const StudentLogin = () => {
                 // Handling form submission
                 try {
                   const response = await axiosInstance.post('/student/login', values)
-                  setError({ visible: true, type: 'success', msg: response.data.message || 'login successfull' })
+                  setError({ visible: true, type: 'success', msg: response.data.message || 'Login successful' })
+      
+                  setTimeout(() => Navigate('/student/home'),1000);
 
                 } catch (error) {
-
                   if (error.response.status == 400) {
-                    setError({ visible: true, type: 'error', msg: error.response.data.message || ' login failed' })
-
+                    setError({ visible: true, type: 'error', msg: error.response.data.message || 'Login failed' })
                   }
                 }
-
-
-
               }}
             >
               {({ handleSubmit }) => (
@@ -80,6 +90,7 @@ const StudentLogin = () => {
                     <ErrorMessage name="password" component="div" className="text-red-500" />
                   </div>
 
+                  {/* Display error message if error exists */}
                   {error && <span> <BasicAlerts type={error.type} msg={error.msg} /></span>}
                   <div className="button-area w-full flex justify-center">
                     <DefaultButton type="submit" value="Login" classname={'hover:bg-[#0070ff] hover:text-white'} />
@@ -87,7 +98,6 @@ const StudentLogin = () => {
                 </Form>
               )}
             </Formik>
-
             <p className='mt-8 font-newFont text-[#4b4343] text-center'>Do not have an account <a href="/student/signup" className='underline text-blue-800'>Register here</a></p>
           </div>
         </div>
