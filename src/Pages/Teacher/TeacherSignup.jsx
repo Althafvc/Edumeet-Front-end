f0ea8f29470d099a735cbdd8dc10512b5102e894
Althaf vc <althafvellanchola46@gmail.com>
2024-06-11 20:13:29 +0530

    some changes added in the teacher signup

--- src/Pages/Teacher/TeacherSignup.jsx
+++ src/Pages/Teacher/TeacherSignup.jsx
@@ -40,7 +40,7 @@ function TeacherSignup() {
   return (
     <>
       <div className="container w-screen h-screen flex justify-center items-center bg-white">
-        <div className="form-container px-2 md:px-0 rounded-2xl md:w-[55%] md:h-[570px] flex flex-col md:flex-row shadow-customShadow overflow-auto md:overflow-hidden h-auto">
+        <div className="form-container px-2 md:px-0 rounded-2xl md:w-[55%] md:h-[590px] flex flex-col md:flex-row shadow-customShadow overflow-auto md:overflow-hidden h-auto">
           <div className={`welcome-area md:w-[50%] h-[230px] md:h-full flex justify-between md:py-5 items-center flex-col gap-5 py-2 md:gap-16`} style={{backgroundImage:`url(${Teacherbg})`,backgroundPosition:"center" ,backgroundSize:"cover"}}>
             <h2 className="text-[#ffffffef] text-[2rem] font-newFont text-center font-semibold">Welcome to Edumeet</h2>
             <div className="description-area w-full flex justify-center items-center">
@@ -49,10 +49,10 @@ function TeacherSignup() {
           </div>
           <div className="form-area flex flex-col bg-[#163c3a]  md:w-[50%] justify-center pl-8 py-3 gap-6">
             <div className="heading-area flex justify-center">
-              <h2 className="text-[white] font-thin shadow-sm font-customFont text-[1.4rem]">Create your account</h2>
+              <h2 className="text-[white] font-thin shadow-sm font-customFont text-[1.4rem] ">Create your account</h2>
             </div>
             <div className="signupform-area w-full justify-center">
-              <form className="flex pl-10 flex-col gap-3 w-full"  onSubmit={handleSubmit(onSubmit)}>
+              <form className="flex pl-10 flex-col gap-2.5 w-full"  onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-outline flex flex-col gap-1">
                   <label htmlFor="name" className="text-[#ffffffe9] text-[.88rem] font-semibold">Full name</label>
                   <input
@@ -186,6 +186,7 @@ function TeacherSignup() {
                 <div className="button-area w-full flex justify-center">
                   <DefaultButton value='Submit' clicked={null} classname={' bg-[#e3b93e] text-black shadow-none font-bold mb-5'}/>
                 </div>
+                <p className='pl-16 md:pl-[120px] text-gray-300'>Existing member <a href="/teacher/login" className='text-white hover:underline'>Login here</a></p>
               </form> 
             </div>
           </div>
